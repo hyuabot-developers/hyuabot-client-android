e1907d20b9c24c2ef3308de9b24bb9f6daca3451
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-07-22 15:58:24 +0900

    액티비티를 한 폴더로 이동하여 리팩토링

---
+++ app/src/main/java/app/kobuggi/hyuabot/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package app.kobuggi.hyuabot.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.kobuggi.hyuabot.R
+import com.google.android.gms.ads.MobileAds
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
