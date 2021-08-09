0cf853a43393200c94b0f157f1e092a13aa60f2a
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-08-09 17:45:47 +0900

    셔틀버스 도착 정보 안내를 위한 Activity 추가

---
+++ app/src/main/java/app/kobuggi/hyuabot/activity/ShuttleActivity.kt
@@ -0,0 +1,12 @@
+package app.kobuggi.hyuabot.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.kobuggi.hyuabot.R
+
+class ShuttleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shuttle)
+    }
+}
