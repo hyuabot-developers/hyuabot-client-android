f9c7eb0a9a4a788f0a0aea62ac67d311f3773fbd
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-09-17 16:45:24 +0900

    분류 별 전화번호부 갱신 코드 추가

--- app/src/main/java/app/kobuggi/hyuabot/activity/ContactActivity.kt
+++ app/src/main/java/app/kobuggi/hyuabot/activity/ContactActivity.kt
@@ -1,5 +1,6 @@
 package app.kobuggi.hyuabot.activity
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -24,12 +25,17 @@ import com.google.android.material.navigation.NavigationView
 import kotlinx.coroutines.*
 import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
+import kotlin.coroutines.CoroutineContext
 
-class ContactActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class ContactActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, CoroutineScope {
     lateinit var drawerLayout : DrawerLayout
     lateinit var searchView: SearchView
     lateinit var queryResult: ArrayList<DatabaseItem>
     lateinit var contactQueryResultListAdapter: ContactQueryResultListAdapter
+    private lateinit var database : AppDatabase
+    private lateinit var job: Job
+    override val coroutineContext : CoroutineContext
+        get() = Dispatchers.IO + job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,27 +52,50 @@ class ContactActivity : AppCompatActivity(), NavigationView.OnNavigationItemSele
         val navigationView = findViewById<NavigationView>(R.id.contact_navigation_view)
         navigationView.setNavigationItemSelectedListener(this)
 
-        val database = Room.databaseBuilder(this, AppDatabase::class.java, "app.db")
+        database = Room.databaseBuilder(this, AppDatabase::class.java, "app.db")
             .createFromAsset("app.db")
             .build()
 
+        job = Job()
+
         val contactQueryListView = findViewById<RecyclerView>(R.id.contact_card_list)
-        val getQueryAllItems = GlobalScope.launch {
+        queryResult = ArrayList()
+        contactQueryResultListAdapter = ContactQueryResultListAdapter(queryResult, this@ContactActivity)
+        contactQueryListView.layoutManager = LinearLayoutManager(this@ContactActivity, RecyclerView.VERTICAL, false)
+        contactQueryListView.adapter = contactQueryResultListAdapter
+        updateQueryResultByCategory("")
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateQueryResultByCategory(category: String){
+        launch {
             queryResult = ArrayList()
-            for(item : DatabaseItem in database.databaseHelper()!!.getPhoneNumberAll()){
-                if(item.phoneNumber!!.trim().isNotEmpty()){
-                    Log.d("Phone", item.phoneNumber.toString())
-                    queryResult.add(item)
+            if(category.isEmpty()){
+                for(item : DatabaseItem in database.databaseHelper()!!.getPhoneNumberAll()){
+                    if(item.phoneNumber!!.trim().isNotEmpty()){
+                        queryResult.add(item)
+                    }
+                }
+            } else if(category == "restaurant"){
+                for(item : DatabaseItem in database.databaseHelper()!!.getPhoneNumberAllRestaurant()){
+                    if(item.phoneNumber!!.trim().isNotEmpty()){
+                        queryResult.add(item)
+                    }
                 }
             }
-            contactQueryResultListAdapter = ContactQueryResultListAdapter(queryResult, this@ContactActivity)
-            contactQueryListView.layoutManager = LinearLayoutManager(this@ContactActivity, RecyclerView.VERTICAL, false)
-            contactQueryListView.adapter = contactQueryResultListAdapter
-            Log.d("Count of result", queryResult.size.toString())
-        }
+            else{
+                for(item : DatabaseItem in database.databaseHelper()!!.getPhoneNumberByCategory(category)){
+                    if(item.phoneNumber!!.trim().isNotEmpty()){
+                        queryResult.add(item)
+                    }
+                }
+            }
 
-        runBlocking {
-            getQueryAllItems.join()
+            withContext(Dispatchers.Main){
+                Log.d("Phone", queryResult.size.toString())
+                contactQueryResultListAdapter.replaceTo(queryResult)
+                contactQueryResultListAdapter.notifyDataSetChanged()
+            }
         }
     }
 
@@ -92,21 +121,27 @@ class ContactActivity : AppCompatActivity(), NavigationView.OnNavigationItemSele
         return false
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.contact_on_campus -> {
+                updateQueryResultByCategory("on campus")
                 Toast.makeText(this, "교내 기관 목록입니다.", Toast.LENGTH_SHORT).show()
             }
             R.id.contact_restaurant -> {
+                updateQueryResultByCategory("restaurant")
                 Toast.makeText(this, "교외 식당 목록입니다.", Toast.LENGTH_SHORT).show()
             }
             R.id.contact_bakery -> {
+                updateQueryResultByCategory("bakery")
                 Toast.makeText(this, "교외 빵집 목록입니다.", Toast.LENGTH_SHORT).show()
             }
             R.id.contact_cafe -> {
+                updateQueryResultByCategory("cafe")
                 Toast.makeText(this, "교외 카페 목록입니다.", Toast.LENGTH_SHORT).show()
             }
             R.id.contact_pub -> {
+                updateQueryResultByCategory("pub")
                 Toast.makeText(this, "교외 주점 목록입니다.", Toast.LENGTH_SHORT).show()
             }
         }
@@ -120,5 +155,10 @@ class ContactActivity : AppCompatActivity(), NavigationView.OnNavigationItemSele
         } else {
             super.onBackPressed()
         }
+    }
+
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
     }
 }
--- app/src/main/java/app/kobuggi/hyuabot/function/DatabaseHelper.kt
+++ app/src/main/java/app/kobuggi/hyuabot/function/DatabaseHelper.kt
@@ -9,9 +9,12 @@ interface DatabaseHelper {
     @Query("SELECT * FROM app where category = :category")
     fun getMarkersByCategory(category: String) : Array<DatabaseItem>
 
-    @Query("SELECT * FROM app where category = :category and name like :keyword and phone is not null or phone != \"\"")
-    fun getPhoneNumber(category: String, keyword: String) : Array<DatabaseItem>
+    @Query("SELECT * FROM app where category = :category and phone is not null")
+    fun getPhoneNumberByCategory(category: String) : Array<DatabaseItem>
 
     @Query("SELECT * FROM app where phone is not null")
     fun getPhoneNumberAll() : Array<DatabaseItem>
+
+    @Query("SELECT * FROM app where (category=\"korean\" or category=\"japanese\" or category=\"chinese\" or category=\"western\" or category=\"fast food\" or category=\"chicken\" or category=\"pizza\" or category=\"meat\" or category=\"other food\") and phone is not null")
+    fun getPhoneNumberAllRestaurant() : Array<DatabaseItem>
 }
