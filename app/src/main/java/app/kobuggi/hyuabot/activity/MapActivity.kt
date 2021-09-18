edcdb6c07295db340ea8a9f78fac60dbd2ed4eeb
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-09-18 18:33:53 +0900

    효과적인 마커 클러스터링을 위한 지도 타입 변경

--- app/src/main/java/app/kobuggi/hyuabot/activity/MapActivity.kt
+++ app/src/main/java/app/kobuggi/hyuabot/activity/MapActivity.kt
@@ -2,9 +2,11 @@ package app.kobuggi.hyuabot.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.HorizontalScrollView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.isVisible
 import androidx.room.Room
@@ -18,7 +20,9 @@ import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.Overlay
 import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.MarkerIcons
 import kotlinx.coroutines.*
@@ -29,11 +33,13 @@ import kotlin.coroutines.CoroutineContext
 class MapActivity : AppCompatActivity(), CoroutineScope, OnMapReadyCallback {
     private val markers = arrayListOf<MarkerItem>()
     private val databaseItems = arrayListOf<DatabaseItem>()
+    private lateinit var infoWindow : InfoWindow
 
     private lateinit var job: Job
     private lateinit var database : AppDatabase
     private val mapView : MapView by lazy { findViewById(R.id.map_view) }
     private lateinit var map : NaverMap
+    lateinit var tedNaverClustering: TedNaverClustering<MarkerItem>
 
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.IO + job
@@ -106,20 +112,15 @@ class MapActivity : AppCompatActivity(), CoroutineScope, OnMapReadyCallback {
                 val marker = MarkerItem(item.name, item.category, item.description, LatLng(item.latitude!!, item.longitude!!))
                 markers.add(marker)
             }
+
+
             withContext(Main){
-                TedNaverClustering.with<MarkerItem>(this@MapActivity, map)
-                    .items(markers)
-                    .customMarker{
-                        clusterItem: MarkerItem -> Marker(clusterItem.position).apply {
-                            captionText = clusterItem.name
-                            icon = OverlayImage.fromResource(R.drawable.marker_campus_building)
-                        }
-                    }
-                    .make()
+                tedNaverClustering.clearItems()
+                tedNaverClustering.addItems(markers)
                 // 지도 위치 이동
                 val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.30016859443061, 126.83779653945606))
                 map.moveCamera(cameraUpdate)
-                map.moveCamera(CameraUpdate.zoomTo(15.0))
+                map.moveCamera(CameraUpdate.zoomTo(16.0))
             }
         }
     }
@@ -161,19 +162,41 @@ class MapActivity : AppCompatActivity(), CoroutineScope, OnMapReadyCallback {
 
     override fun onMapReady(naverMap: NaverMap) {
         map = naverMap
-        map.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, false)
-        map.symbolScale = 0F
+        infoWindow = InfoWindow()
         // 지도 다 로드 이후에 가져오기
         updateMarkersByCategory("building")
-        // 줌 범위 설정
-        naverMap.maxZoom = 18.0
-        naverMap.minZoom = 10.0
-
+        naverMap.mapType = NaverMap.MapType.Satellite
         naverMap.minZoom = 14.0
         naverMap.maxZoom = 18.0
         // 현위치 버튼 기능
         val uiSetting = naverMap.uiSettings
         uiSetting.isLocationButtonEnabled = false // 뷰 페이져에 가려져 이후 레이아웃에 정의 하였음.
         uiSetting.isZoomControlEnabled = false
+
+        tedNaverClustering = TedNaverClustering.with<MarkerItem>(this, naverMap)
+            .customMarker{
+                Marker().apply {
+                    width = 60
+                    height = 60
+                    captionText = it.name
+                    icon = OverlayImage.fromResource(R.drawable.marker_campus_building)
+                    isHideCollidedSymbols = true
+                    isHideCollidedMarkers = true
+                    isHideCollidedCaptions = true
+                }
+            }
+            .markerClickListener { markerItem ->
+                val position = markerItem.position
+                Toast.makeText(
+                    this,
+                    "${position.latitude},${position.longitude} 클릭됨",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .make()
+        naverMap.addOnCameraChangeListener { _, _ ->
+            Log.d(
+                "Current Zoom", naverMap.cameraPosition.zoom.toString())
+        }
     }
 }
