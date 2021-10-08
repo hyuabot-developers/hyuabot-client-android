abd5504b96853ce15df513e1fd0b86330d33dec1
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-10-08 12:04:06 +0900

    Remove export schema warning

--- app/src/main/java/app/kobuggi/hyuabot/function/AppDatabase.kt
+++ app/src/main/java/app/kobuggi/hyuabot/function/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import app.kobuggi.hyuabot.model.DatabaseItem
 
-@Database(entities = [DatabaseItem::class], version = 1)
+@Database(entities = [DatabaseItem::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun databaseHelper(): DatabaseHelper?
 }
