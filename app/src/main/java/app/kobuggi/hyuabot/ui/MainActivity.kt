fbaad62dfc674a48f0f85fb535420689cf14e284
Jeongin Lee <jil8885@hanyang.ac.kr>
2024-04-15 20:33:00 +0900

    feat: Implement bottom navigation

--- app/src/main/java/app/kobuggi/hyuabot/ui/MainActivity.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/MainActivity.kt
@@ -2,15 +2,24 @@ package app.kobuggi.hyuabot.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import app.kobuggi.hyuabot.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id)!! as NavHostFragment
+        navHostFragment.navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
