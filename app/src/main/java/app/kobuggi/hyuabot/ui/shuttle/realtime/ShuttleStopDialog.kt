f517947a2d857f19bcb25e26c114502a724595b5
Jeongin Lee <jil8885@hanyang.ac.kr>
2024-04-23 17:14:39 +0900

    feat: Implement first / last shuttle

--- app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/realtime/ShuttleStopDialog.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/realtime/ShuttleStopDialog.kt
@@ -18,14 +18,11 @@ import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
-import com.kakao.vectormap.camera.CameraUpdate
 import com.kakao.vectormap.camera.CameraUpdateFactory
-import com.kakao.vectormap.label.LabelLayerOptions
 import com.kakao.vectormap.label.LabelOptions
 import com.kakao.vectormap.label.LabelStyle
 import com.kakao.vectormap.label.LabelStyles
 import dagger.hilt.android.AndroidEntryPoint
-import java.lang.Exception
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -120,6 +117,97 @@ class ShuttleStopDialog @Inject constructor() : BottomSheetDialogFragment() {
                 }
             }
         }
+
+        viewModel.departureList.observe(viewLifecycleOwner) {
+            when (stopID) {
+                R.string.shuttle_tab_dormitory_out, R.string.shuttle_tab_shuttlecock_out -> {
+                    val departureList = it.sortedBy { item -> item.time }
+                    val weekdaysStationDepartureList = departureList.filter { item -> item.weekdays && (item.tag == "C" || item.tag == "DH" || item.tag == "DJ") }
+                    val weekendsStationDepartureList = departureList.filter { item -> !item.weekdays && (item.tag == "C" || item.tag == "DH" || item.tag == "DJ") }
+                    val weekdaysTerminalDepartureList = departureList.filter { item -> item.weekdays && (item.tag == "C" || item.tag == "DY") }
+                    val weekendsTerminalDepartureList = departureList.filter { item -> !item.weekdays && (item.tag == "C" || item.tag == "DY") }
+                    val weekdaysJungangStationDepartureList = departureList.filter { item -> item.weekdays && item.tag == "DJ" }
+                    val weekendsJungangStationDepartureList = departureList.filter { item -> !item.weekdays && item.tag == "DJ" }
+                    binding.apply {
+                        if (weekdaysStationDepartureList.isNotEmpty()) {
+                            shuttleForStationFirstTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysStationDepartureList[0].time.split(":")[0], weekdaysStationDepartureList[0].time.split(":")[1])
+                            shuttleForStationLastTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysStationDepartureList[weekdaysStationDepartureList.size - 1].time.split(":")[0], weekdaysStationDepartureList[weekdaysStationDepartureList.size - 1].time.split(":")[1])
+                        }
+                        if (weekendsStationDepartureList.isNotEmpty()) {
+                            shuttleForStationFirstTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsStationDepartureList[0].time.split(":")[0], weekendsStationDepartureList[0].time.split(":")[1])
+                            shuttleForStationLastTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsStationDepartureList[weekendsStationDepartureList.size - 1].time.split(":")[0], weekendsStationDepartureList[weekendsStationDepartureList.size - 1].time.split(":")[1])
+                        }
+                        if (weekdaysTerminalDepartureList.isNotEmpty()) {
+                            shuttleForTerminalFirstTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysTerminalDepartureList[0].time.split(":")[0], weekdaysTerminalDepartureList[0].time.split(":")[1])
+                            shuttleForTerminalLastTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysTerminalDepartureList[weekdaysTerminalDepartureList.size - 1].time.split(":")[0], weekdaysTerminalDepartureList[weekdaysTerminalDepartureList.size - 1].time.split(":")[1])
+                        }
+                        if (weekendsTerminalDepartureList.isNotEmpty()) {
+                            shuttleForTerminalFirstTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsTerminalDepartureList[0].time.split(":")[0], weekendsTerminalDepartureList[0].time.split(":")[1])
+                            shuttleForTerminalLastTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsTerminalDepartureList[weekendsTerminalDepartureList.size - 1].time.split(":")[0], weekendsTerminalDepartureList[weekendsTerminalDepartureList.size - 1].time.split(":")[1])
+                        }
+                        if (weekdaysJungangStationDepartureList.isNotEmpty()) {
+                            shuttleForJungangStationFirstTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysJungangStationDepartureList[0].time.split(":")[0], weekdaysJungangStationDepartureList[0].time.split(":")[1])
+                            shuttleForJungangStationLastTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysJungangStationDepartureList[weekdaysJungangStationDepartureList.size - 1].time.split(":")[0], weekdaysJungangStationDepartureList[weekdaysJungangStationDepartureList.size - 1].time.split(":")[1])
+                        }
+                        if (weekendsJungangStationDepartureList.isNotEmpty()) {
+                            shuttleForJungangStationFirstTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsJungangStationDepartureList[0].time.split(":")[0], weekendsJungangStationDepartureList[0].time.split(":")[1])
+                            shuttleForJungangStationLastTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsJungangStationDepartureList[weekendsJungangStationDepartureList.size - 1].time.split(":")[0], weekendsJungangStationDepartureList[weekendsJungangStationDepartureList.size - 1].time.split(":")[1])
+                        }
+                    }
+                }
+                R.string.shuttle_tab_station -> {
+                    val departureList = it.sortedBy { item -> item.time }
+                    val weekdaysDormitoryDepartureList = departureList.filter { item -> item.weekdays }
+                    val weekendsDormitoryDepartureList = departureList.filter { item -> !item.weekdays }
+                    val weekdaysTerminalDepartureList = departureList.filter { item -> item.weekdays && item.tag == "C" }
+                    val weekendsTerminalDepartureList = departureList.filter { item -> !item.weekdays && item.tag == "C" }
+                    val weekdaysJungangStationDepartureList = departureList.filter { item -> item.weekdays && item.tag == "DJ" }
+                    val weekendsJungangStationDepartureList = departureList.filter { item -> !item.weekdays && item.tag == "DJ" }
+                    binding.apply {
+                        if (weekdaysDormitoryDepartureList.isNotEmpty()) {
+                            shuttleForDormitoryFirstTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysDormitoryDepartureList[0].time.split(":")[0], weekdaysDormitoryDepartureList[0].time.split(":")[1])
+                            shuttleForDormitoryLastTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysDormitoryDepartureList[weekdaysDormitoryDepartureList.size - 1].time.split(":")[0], weekdaysDormitoryDepartureList[weekdaysDormitoryDepartureList.size - 1].time.split(":")[1])
+                        }
+                        if (weekendsDormitoryDepartureList.isNotEmpty()) {
+                            shuttleForDormitoryFirstTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsDormitoryDepartureList[0].time.split(":")[0], weekendsDormitoryDepartureList[0].time.split(":")[1])
+                            shuttleForDormitoryLastTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsDormitoryDepartureList[weekendsDormitoryDepartureList.size - 1].time.split(":")[0], weekendsDormitoryDepartureList[weekendsDormitoryDepartureList.size - 1].time.split(":")[1])
+                        }
+                        if (weekdaysTerminalDepartureList.isNotEmpty()) {
+                            shuttleForTerminalFirstTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysTerminalDepartureList[0].time.split(":")[0], weekdaysTerminalDepartureList[0].time.split(":")[1])
+                            shuttleForTerminalLastTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysTerminalDepartureList[weekdaysTerminalDepartureList.size - 1].time.split(":")[0], weekdaysTerminalDepartureList[weekdaysTerminalDepartureList.size - 1].time.split(":")[1])
+                        }
+                        if (weekendsTerminalDepartureList.isNotEmpty()) {
+                            shuttleForTerminalFirstTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsTerminalDepartureList[0].time.split(":")[0], weekendsTerminalDepartureList[0].time.split(":")[1])
+                            shuttleForTerminalLastTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsTerminalDepartureList[weekendsTerminalDepartureList.size - 1].time.split(":")[0], weekendsTerminalDepartureList[weekendsTerminalDepartureList.size - 1].time.split(":")[1])
+                        }
+                        if (weekdaysJungangStationDepartureList.isNotEmpty()) {
+                            shuttleForJungangStationFirstTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysJungangStationDepartureList[0].time.split(":")[0], weekdaysJungangStationDepartureList[0].time.split(":")[1])
+                            shuttleForJungangStationLastTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysJungangStationDepartureList[weekdaysJungangStationDepartureList.size - 1].time.split(":")[0], weekdaysJungangStationDepartureList[weekdaysJungangStationDepartureList.size - 1].time.split(":")[1])
+                        }
+                        if (weekendsJungangStationDepartureList.isNotEmpty()) {
+                            shuttleForJungangStationFirstTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsJungangStationDepartureList[0].time.split(":")[0], weekendsJungangStationDepartureList[0].time.split(":")[1])
+                            shuttleForJungangStationLastTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsJungangStationDepartureList[weekendsJungangStationDepartureList.size - 1].time.split(":")[0], weekendsJungangStationDepartureList[weekendsJungangStationDepartureList.size - 1].time.split(":")[1])
+                        }
+                    }
+                }
+                else -> {
+                    val departureList = it.sortedBy { item -> item.time }
+                    val weekdaysDepartureList = departureList.filter { item -> item.weekdays }
+                    val weekendsDepartureList = departureList.filter { item -> !item.weekdays }
+                    binding.apply {
+                        if (weekdaysDepartureList.isNotEmpty()) {
+                            shuttleForDormitoryFirstTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysDepartureList[0].time.split(":")[0], weekdaysDepartureList[0].time.split(":")[1])
+                            shuttleForDormitoryLastTimeWeekdays.text = getString(R.string.shuttle_first_last_time_format_weekdays, weekdaysDepartureList[weekdaysDepartureList.size - 1].time.split(":")[0], weekdaysDepartureList[weekdaysDepartureList.size - 1].time.split(":")[1])
+                        }
+                        if (weekendsDepartureList.isNotEmpty()) {
+                            shuttleForDormitoryFirstTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsDepartureList[0].time.split(":")[0], weekendsDepartureList[0].time.split(":")[1])
+                            shuttleForDormitoryLastTimeWeekends.text = getString(R.string.shuttle_first_last_time_format_weekends, weekendsDepartureList[weekendsDepartureList.size - 1].time.split(":")[0], weekendsDepartureList[weekendsDepartureList.size - 1].time.split(":")[1])
+                        }
+                    }
+                }
+            }
+        }
+
         return binding.root
     }
 
--- app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/realtime/ShuttleStopDialogViewModel.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/realtime/ShuttleStopDialogViewModel.kt
@@ -7,17 +7,23 @@ import app.kobuggi.hyuabot.ShuttleStopDialogQuery
 import com.apollographql.apollo3.ApolloClient
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @HiltViewModel
 class ShuttleStopDialogViewModel @Inject constructor(private val apolloClient: ApolloClient): ViewModel() {
     private val _result = MutableLiveData<ShuttleStopDialogQuery.Stop?>()
+    private val _departureList = MutableLiveData<List<ShuttleStopDialogQuery.Timetable>>()
     val result get() = _result
+    val departureList get() = _departureList
 
     fun fetchData(stopID: String) {
+        val now = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now())
         viewModelScope.launch {
-            val response = apolloClient.query(ShuttleStopDialogQuery(stopID)).execute()
+            val response = apolloClient.query(ShuttleStopDialogQuery(stopID, now)).execute()
             _result.value = response.data?.shuttle?.stop?.get(0)
+            _departureList.value = response.data?.shuttle?.timetable
         }
     }
 }
