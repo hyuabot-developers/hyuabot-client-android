8ac47b4c99b28fa3ac732b3943beeac82eba4bbd
Jeongin Lee <jil8885@hanyang.ac.kr>
2024-07-05 16:27:41 -0700

    feat: Implement FCM notification

--- app/src/main/java/app/kobuggi/hyuabot/service/NotificationService.kt
+++ app/src/main/java/app/kobuggi/hyuabot/service/NotificationService.kt
@@ -1,6 +1,14 @@
 package app.kobuggi.hyuabot.service
 
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationManagerCompat
+import app.kobuggi.hyuabot.R
 import app.kobuggi.hyuabot.service.preferences.UserPreferencesRepository
 import com.google.firebase.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -27,5 +35,24 @@ class NotificationService : FirebaseMessagingService() {
         }.addOnFailureListener {
             Log.e("NotificationService", "Failed to unsubscribe from reading room $topic")
         }
+
+        val notificationChannel = NotificationChannel(CHANNEL_ID, "Reading Room Notification", NotificationManager.IMPORTANCE_DEFAULT)
+        getSystemService(NotificationManager::class.java).createNotificationChannel(notificationChannel)
+        val notification = Notification.Builder(this, CHANNEL_ID)
+            .setGroup(NOTIFICATION_GROUP_ID)
+            .setContentTitle(getString(R.string.reading_room_noti_title))
+            .setContentText(message.data["body"])
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .build()
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        NotificationManagerCompat.from(this).notify(topic.toInt(), notification)
+    }
+
+    companion object {
+        const val CHANNEL_ID = "ReadingRoomNotification"
+        const val NOTIFICATION_GROUP_ID = "app.kobuggi.hyuabot.reading_room_notification"
     }
 }
