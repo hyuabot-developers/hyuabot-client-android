6cff1b46f5175d1c2edf9662e8e58bc27da2c34b
Jeongin Lee <jil8885@gmail.com>
2025-03-04 19:35:53 +0900

    feat: Implement shuttle via dialog for realtime

--- app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/via/ShuttleViaSheetDialog.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/via/ShuttleViaSheetDialog.kt
@@ -4,19 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
+import androidx.recyclerview.widget.LinearLayoutManager
 import app.kobuggi.hyuabot.ShuttleRealtimePageQuery
+import app.kobuggi.hyuabot.ShuttleTimetablePageQuery
 import app.kobuggi.hyuabot.databinding.DialogShuttleViaBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ShuttleViaSheetDialog(
-    private val viaStops: List<ShuttleRealtimePageQuery.Vium>
+    private val realtimeViaStops: List<ShuttleRealtimePageQuery.Vium> = listOf(),
+    private val timetableViaStops: List<ShuttleTimetablePageQuery.Vium> = listOf()
 ): BottomSheetDialogFragment() {
     private val binding by lazy { DialogShuttleViaBinding.inflate(layoutInflater) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding.toolbar.setOnMenuItemClickListener {
-            dismiss()
-            true
+        val decoration = DividerItemDecoration(requireContext(), VERTICAL)
+        binding.apply {
+            toolbar.setOnMenuItemClickListener {
+                dismiss()
+                true
+            }
+            viaList.apply {
+                adapter = ShuttleViaListAdapter(requireContext(), realtimeViaStops, timetableViaStops)
+                layoutManager = LinearLayoutManager(requireContext())
+                addItemDecoration(decoration)
+            }
         }
         return binding.root
     }
