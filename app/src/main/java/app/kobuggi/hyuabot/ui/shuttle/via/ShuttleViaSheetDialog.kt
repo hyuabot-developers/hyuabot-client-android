fa4954637423834bf718ee7a0cbcd3aa54d7d638
Jeongin Lee <jil8885@gmail.com>
2025-03-04 18:07:26 +0900

    feat: Open shuttle via dialog when clicked

--- app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/via/ShuttleViaSheetDialog.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/via/ShuttleViaSheetDialog.kt
@@ -5,15 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import app.kobuggi.hyuabot.ShuttleRealtimePageQuery
-import app.kobuggi.hyuabot.databinding.SheetShuttleViaBinding
+import app.kobuggi.hyuabot.databinding.DialogShuttleViaBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ShuttleViaSheetDialog(
     private val viaStops: List<ShuttleRealtimePageQuery.Vium>
 ): BottomSheetDialogFragment() {
-    private val binding by lazy { SheetShuttleViaBinding.inflate(layoutInflater) }
+    private val binding by lazy { DialogShuttleViaBinding.inflate(layoutInflater) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding.toolbar.setOnMenuItemClickListener {
+            dismiss()
+            true
+        }
         return binding.root
     }
 }
