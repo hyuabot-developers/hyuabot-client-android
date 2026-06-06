59deaebd387fda71956712cc2403dbf75ea82c69
Jeongin Lee <jil8885@gmail.com>
2026-06-06 18:24:58 +0900

    chore: Version Upgrade (5.1.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "app.kobuggi.hyuabot"
         minSdk = 29
         targetSdk = 37
-        versionCode = 511000000
-        versionName = "5.1.1"
+        versionCode = 512000000
+        versionName = "5.1.2"
         signingConfig = signingConfigs.getByName("config")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["MAPS_API_KEY"] = props["GOOGLE_MAP_API_KEY"]?.toString() ?: ""
