1a6a354d15a6d6dfa0c886afb881abef947b2fc8
Jeongin Lee <jil8885@gmail.com>
2026-07-05 18:41:20 +0900

    fix: Expand shuttle help dialog

--- app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/realtime/ShuttleHelpDialog.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/realtime/ShuttleHelpDialog.kt
@@ -20,6 +20,6 @@ class ShuttleHelpDialog @Inject constructor() : DialogFragment() {
 
     override fun onResume() {
         super.onResume()
-        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 }
