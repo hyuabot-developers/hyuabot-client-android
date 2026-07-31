5c7dd48502676bd01714d3e82e9fdafc72a0fe90
Jeongin Lee <jil8885@gmail.com>
2026-07-31 13:40:52 +0900

    Bump app version to 5.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "app.kobuggi.hyuabot"
         minSdk = 29
         targetSdk = 37
-        versionCode = 520000000
-        versionName = "5.2.0"
+        versionCode = 521000000
+        versionName = "5.2.1"
         signingConfig = signingConfigs.getByName("config")
         manifestPlaceholders["MAP_CLIENT_ID"] = props["MAP_CLIENT_ID"]?.toString() ?: ""
     }
